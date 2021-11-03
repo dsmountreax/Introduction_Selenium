a89bacfc116899be25a2824fbdf8c1d621f2ac3c
dsmountreax <dsmountreax@gmail.com>
2021-11-03 11:16:21 -0500

    "Update 03/11/2021"

--- src/section08/Lab064.java
+++ src/section08/Lab064.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Lab064 {
 	final static String url1 = "https://rahulshettyacademy.com/AutomationPractice/";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		// TODO Auto-generated method stub
 		String text = "Enrique";
+		String path=System.getProperty("user.dir");
+		String pathChrome=path+"\\drivers\\Chrome\\chromedriver.exe";
 
-		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver89\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", pathChrome);
 
 		WebDriver driver = new ChromeDriver();
 
@@ -21,10 +23,14 @@ public class Lab064 {
 		driver.findElement(By.cssSelector("input#alertbtn.btn-style")).click();
 
 		// Snipet sysout then ctrl+space
+		Thread.sleep(3000);
 		System.out.println(driver.switchTo().alert().getText());
 		driver.switchTo().alert().accept();
+
 		driver.findElement(By.cssSelector("input#confirmbtn.btn-style")).click();
 		System.out.println(driver.switchTo().alert().getText());
+		Thread.sleep(3000);
 		driver.switchTo().alert().dismiss();
+		driver.close();
 	}
 }
